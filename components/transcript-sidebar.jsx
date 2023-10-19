619056e441a4acd41f01ec4dfa0cf9268905b101
Ivy Rose <hi@ivyrose.me>
2023-10-18 20:09:53 -0400

    Readded scroll styling with intersection observer

--- components/transcript-sidebar.jsx
+++ components/transcript-sidebar.jsx
@@ -1,12 +1,16 @@
-import React, { useState, useEffect } from "react";
+import React, { useRef, useState, useEffect } from "react";
 import { WebVTT } from "videojs-vtt.js";
 import cx from "classnames";
 
 export const TranscriptSidebar = props => {
     const { textTracks, defaultTrack, playheadTime, onSeek } = props;
 
+    const firstCueRef = useRef();
+    const trackContainerRef = useRef();
+
     const [ selectedTrack, setSelectedTrack ] = useState(defaultTrack);
     const [ cues, setCues ] = useState([]);
+    const [ isScrolled, setScrolled ] = useState(false);
     const [ isClosed, setClosed ] = useState(false);
 
     useEffect(() => {
@@ -29,13 +33,31 @@ export const TranscriptSidebar = props => {
         vttFetch();
     }, [ textTracks, selectedTrack ]);
 
+    useEffect(() => {
+        if (!firstCueRef?.current || !trackContainerRef?.current) { return; }
+
+        const callback = entries => {
+            setScrolled(!entries[0].isIntersecting);
+        };
+
+        const observer = new IntersectionObserver(callback, {
+            root: trackContainerRef.current,
+            threshold: 0.75
+        });
+        observer.observe(firstCueRef.current);
+
+        return () => observer.unobserve(firstCueRef.current);
+    }, [ cues ]);
+
     if (isClosed) { return null; }
 
     return (
         <div className={ cx(
             "player-sidebar", { "loading": !cues.length }
         ) } >
-            <div className="player-header">
+            <div className={ cx(
+                "player-header", { "scrolled": isScrolled }
+            ) }>
                 <select
                     value={ selectedTrack }
                     onChange={ event => setSelectedTrack(event.target.value) }
@@ -56,17 +78,21 @@ export const TranscriptSidebar = props => {
                     onClick={ () => setClosed(true) }
                 />
             </div>
-            <div className="player-track-container">
+            <div
+                ref={ trackContainerRef }
+                className="player-track-container"
+            >
                 <div
                     className="player-track active"
                     lang={ selectedTrack }
                 >
-                    { cues.map(cue => {
+                    { cues.map((cue, index) => {
                         const isActive = cue.startTime <= playheadTime &&
                             cue.endTime >= playheadTime;
 
                         return (
                             <p
+                                ref={ index === 0 ? firstCueRef : undefined }
                                 className={ isActive ? "active" : "" }
                                 onClick={ () => onSeek(cue.startTime + 0.1) }
                                 key={ cue.text }
